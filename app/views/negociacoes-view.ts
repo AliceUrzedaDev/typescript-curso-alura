a54f2b6991cf68613308aab088d903d8d3aeded0
AliceUrzedaDev <aliceferreiradev@gmail.com>
2023-08-24 14:47:36 -0300

    adicionando a file view

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacoesView extends View {
+export class NegociacoesView extends View<Negociacoes> {
 
     template(model: Negociacoes): string {
         return `
@@ -26,11 +26,5 @@ export class NegociacoesView extends View {
             </tbody>
         </table)
         `;
-    }
-
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
     }
 }
