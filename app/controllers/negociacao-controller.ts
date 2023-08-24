3d8864825aafbb3b4b9cff1ac3d65b885b719e04
AliceUrzedaDev <aliceferreiradev@gmail.com>
2023-08-24 16:32:57 -0300

    criando outra class criaDe

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -22,7 +22,12 @@ export class NegociacaoController {
   }
 
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacaoTemp = new Negociacao(null, 0, 0)
+    const negociacao = negociacaoTemp.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
     if (!this.eDiaUtil(negociacao.data)) {
       this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
       return ;
@@ -35,14 +40,6 @@ export class NegociacaoController {
 
   private eDiaUtil(data: Date) {
     return data.getDay() > DiasDaSemana.DOMINGO && data.getDay() < DiasDaSemana.SABADO;
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ","));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
