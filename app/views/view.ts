c3ae64f47874d2b9f86776f9c9e30140981651f2
AliceUrzedaDev <aliceferreiradev@gmail.com>
2023-08-31 14:07:27 -0300

    ultimos ajustes no codigo

--- app/views/view.ts
+++ app/views/view.ts
@@ -4,12 +4,17 @@ export abstract class View<T> {
     private escapar = false;
 
     constructor(seletor: string, escapar?: boolean) {
-        this.elemento = document.querySelector(seletor);
+        const elemento = document.querySelector(seletor);
+        if (elemento) {
+            this.elemento = elemento as HTMLElement;
+        } else {
+            throw Error (`Seletor ${seletor} não existe no DOM. Verifique`)
+        }
         if (escapar) {
             this.escapar = escapar;
         }
     }
-
+    
     public update(model: T): void {
         let template = this.template(model);
         if (this.escapar) {
