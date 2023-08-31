d9bde8b1a8d0435679bbd227f640ea327b597e0d
AliceUrzedaDev <aliceferreiradev@gmail.com>
2023-08-31 14:03:12 -0300

    Mudança no tsconfig.json e tratando null do codigo

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -15,9 +15,9 @@ export class NegociacaoController {
   private readonly DOMINGO = 0;
 
   constructor() {
-    this.inputData = document.querySelector("#data");
-    this.inputQuantidade = document.querySelector("#quantidade");
-    this.inputValor = document.querySelector("#valor");
+    this.inputData = document.querySelector("#data") as HTMLInputElement;
+    this.inputQuantidade = document.querySelector("#quantidade") as HTMLInputElement;
+    this.inputValor = document.querySelector("#valor") as HTMLInputElement;
     this.negociacoesView.update(this.negociacoes);
   }
 
